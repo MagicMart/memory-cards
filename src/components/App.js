5cfc8e77aa5470ee39513ca30bfaf48099b37d7a
Martin Tudor <martcan27@gmail.com>
2019-10-25 20:26:58 +0100

    add styled components

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@
 
 import React from "react";
 import Card from "./Card";
+import styled from "styled-components";
 import {
     FaBug,
     FaCoffee,
@@ -32,19 +33,28 @@ const iconsArr = [
     FaCode
 ];
 
+const Container = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    width: 600px;
+    background: green;
+    margin: 0 auto;
+`;
+
 const shuffleArr = arr => [...arr.sort(() => 0.5 - Math.random())];
 
 function App() {
     const [icons, setIcons] = React.useState(() => shuffleArr(iconsArr));
     return (
-        <div>
+        <Container>
             {icons.map((icon, i) => (
                 <Card key={i + icon} Icon={icon} />
             ))}
             <button onClick={() => setIcons(icons => shuffleArr(icons))}>
                 Shuffle
             </button>
-        </div>
+        </Container>
     );
 }
 
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,17 @@
 // @flow
 
 import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+    width: 24%;
+`;
 
 function Card({Icon}: {Icon: Object}) {
     return (
-        <div>
+        <Container>
             <Icon size={40} />
-        </div>
+        </Container>
     );
 }
 
