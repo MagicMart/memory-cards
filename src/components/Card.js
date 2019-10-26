01d4eb783196abbc7eb0acacfb1fb4cea10502ce
Martin Tudor <martcan27@gmail.com>
2019-10-26 21:11:24 +0100

    animations

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,30 @@
 // @flow
 
 import React from "react";
-import styled from "styled-components";
+import styled, {keyframes} from "styled-components";
+
+const faceUp = keyframes`
+  from {
+    transform: rotateY(0deg);
+  }
+
+  to {
+    transform: rotateY(180deg);
+  }
+`;
+
+const faceDown = keyframes`
+  from {
+    transform: rotateY(180deg);
+  }
+
+  to {
+    transform: rotateY(0deg);
+  }
+`;
 
 const StyledCard = styled.div`
+    animation: ${props => (props.open ? faceUp : faceDown)} 0.5s linear;
     display: flex;
     justify-content: center;
     align-items: center;
@@ -11,15 +32,23 @@ const StyledCard = styled.div`
     height: 23%;
     margin: 1%;
     background: #02b3e4;
-    color: black;
     border-radius: 15px;
-    box-shadow: 5px 5px 5px grey;
+    border: 1px solid grey;
+    .icon {
+        animation: inherit;
+    }
 `;
 
 function Card({Icon}: {Icon: Object}) {
+    const [openCard, setOpenCard] = React.useState(false);
+    console.log("render");
     return (
-        <StyledCard>
-            <Icon size={45} />
+        <StyledCard open={openCard} onClick={() => setOpenCard(c => !c)}>
+            <Icon
+                className="icon"
+                size={45}
+                color={openCard ? "white" : "#02b3e4"}
+            />
         </StyledCard>
     );
 }
