8b28a783758645328e5a96fa9de3aec107d0de48
Martin Tudor <martcan27@gmail.com>
2019-11-07 21:02:33 +0000

    update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
-// @flow
-
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
-ReactDOM.render(<App />, window.document.getElementById("app"));
+ReactDOM.render(<App />, document.getElementById("app"));
